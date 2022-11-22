b33914a15927118b0d2270d7e82d191768d80886
saurabhppawar04 <78255828+saurabhppawar04@users.noreply.github.com>
2022-11-22 20:44:40 +0530

    7th

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiWhatsappLine } from "react-icons/ri";
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_vbrpk8r', 'template_2gnko6g', form.current, 'X7VFMxUUJGp82R2VM')
+    
+    e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -14,36 +26,20 @@ const Contact = () => {
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>saurabhppawar0704@gmail.com</h5>
-            <a href="mailto:saurabhppawar0704@gmail.com" target="_blank" rel="noreferrer">
-              Send a message
-            </a>
+            <a href="mailto:saurabhppawar0704@gmail.com" target="_blank" rel="noreferrer">Send a message </a>
           </article>
           <article className="contact__option">
             <RiWhatsappLine className="contact__option-icon" />
             <h4>WhatsApp</h4>
-            <h5>+917066842958</h5>
-            <a href="https://wa.me/+917066842958" target="_blank" rel="noreferrer">
-              Send a message
-            </a>
+            <h5>message me on WhatsApp</h5>
+            <a href="https://wa.me/+917066842958" target="_blank" rel="noreferrer">Send a message </a>
           </article>
         </div>
-        <form>
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Full Name"
-            required
-          />
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
-          <textarea
-            name="message"
-            rows="7"
-            placeholder="Your Message"
-            required
-          ></textarea>
-          <button type="submit" className="btn btn-primary">
-            Send Message
-          </button>
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
         </form>
       </div>
     </section>
